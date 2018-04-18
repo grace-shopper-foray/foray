71d592188d32e739cb31d6ccba0dfffa8f625a3a
Philip_Leung <philip.leung90@gmail.com>
2018-04-17 22:27:08 -0400

    done get , put , post for /api/users

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -12,26 +12,84 @@ describe('User routes', () => {
   })
 
   describe('/api/users/', () => {
-    const codysEmail = 'cody@puppybook.com'
-    const firstName = "Geena"
-    const lastName = "Gao"
+    let geoff
+    let omri
+    beforeEach("Seed Users" , () => {
+      const users = [
+        {
+          firstName: "Geoff",
+          lastName: "hello",
+          email: "geoff@fullstack.com"
+        },
+        {
+          firstName: "omri",
+          lastName: "world",
+          email: "omri@fullstack.com"
+        }
+      ];
+    return User.bulkCreate(users, { returning: true}).then(
+      createdUser => {
+        geoff = createdUser[0].id
+        omri = createdUser[1].id
+      }
+    )
+  })
 
-    beforeEach(() => {
-      return User.create({
-        firstName,
-        lastName,
-        email: codysEmail
+    //test get route
+    describe("users" , ()=>{
+
+      it('GET /api/users', () => {
+        return request(app)
+          .get('/api/users')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an('array')
+            expect(res.body[0].email).to.be.equal("geoff@fullstack.com")
+          })
       })
-    })
 
-    it('GET /api/users', () => {
-      return request(app)
-        .get('/api/users')
-        .expect(200)
-        .then(res => {
-          expect(res.body).to.be.an('array')
-          expect(res.body[0].email).to.be.equal(codysEmail)
-        })
+      //test put route
+      it("updates a user at PUT /{{usersId}}, sending a 201 response", () => {
+        return request(app)
+          .put(`/users/${geoff}`)
+          .send({
+            email: "potus@hotmail.com"
+          })
+          .expect(201)
+          .then(res => {
+            return User.findById(geoff);
+          })
+          .then(user => {
+            expect(user.email).to.be.equal("potus@hotmail.com");
+          });
+      });
+
+      //test post route
+      it('adds a new message on POST /, responding with 201 and created message', () => {
+        return request(app)
+          .post('/api/users')
+          .send({
+            firstName: 'chung',
+            lastName: 'yi',
+            email: 'senior@fullstack.com'
+          })
+          .expect(201)
+          .then(res => {
+            const createdUser = res.body
+            return User.findById(createdUser.id)
+          })
+          .then(foundUser => {
+            expect(foundUser.firstName).to.be.equal("chung")
+            expect(foundUser.lastName).to.be.equal("yi")
+            expect(foundUser.email).to.be.equal("senior@fullstack.com")
+          })
+      })
+
+      // it("delete a user on DELETE / , responding with ", () => {
+      //   return request(app)
+      //     .delete(`api/${}`)
+      // })
+
     })
   }) // end describe('/api/users')
 }) // end describe('User routes')
