78eed3ee73bf4ab4e8e3704a81de4ca09dbd83cb
Philip_Leung <philip.leung90@gmail.com>
2018-04-19 15:04:25 -0400

    signin fixed

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,41 +1,49 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import {auth} from '../store'
+import { auth } from '../store'
 
 /**
  * COMPONENT
  */
-const AuthForm = (props) => {
-  const {name, displayName, handleSubmit, error} = props
+const AuthForm = props => {
+  const { name, displayName, handleSubmit, error } = props
 
   return (
     <div>
       <form onSubmit={handleSubmit} name={name}>
-        {
-          name === 'signup' && (
+        {name === 'signup' && (
+          <div>
             <div>
-              <div>
-                <label htmlFor="firstName"><small>FirstName</small></label>
-                <input name="firstName" type="text" />
-              </div>
-              <div>
-                <label htmlFor="lastName"><small>LastName</small></label>
-                <input name="lastName" type="text" />
-              </div>
-              <div>
-                <label htmlFor="phoneNumber"><small>Phone Number</small></label>
-                <input name="phoneNumber" type="text" />
-              </div>
+              <label htmlFor="firstName">
+                <small>FirstName</small>
+              </label>
+              <input name="firstName" type="text" />
+            </div>
+            <div>
+              <label htmlFor="lastName">
+                <small>LastName</small>
+              </label>
+              <input name="lastName" type="text" />
+            </div>
+            <div>
+              <label htmlFor="phoneNumber">
+                <small>Phone Number</small>
+              </label>
+              <input name="phoneNumber" type="text" />
+            </div>
           </div>
-          )
-        }
+        )}
         <div>
-          <label htmlFor="email"><small>Email</small></label>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
           <input name="email" type="text" />
         </div>
         <div>
-          <label htmlFor="password"><small>Password</small></label>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
           <input name="password" type="password" />
         </div>
         <div>
@@ -55,7 +63,7 @@ const AuthForm = (props) => {
  *   function, and share the same Component. This is a good example of how we
  *   can stay DRY with interfaces that are very similar to each other!
  */
-const mapLogin = (state) => {
+const mapLogin = state => {
   return {
     name: 'login',
     displayName: 'Login',
@@ -63,7 +71,7 @@ const mapLogin = (state) => {
   }
 }
 
-const mapSignup = (state) => {
+const mapSignup = state => {
   return {
     name: 'signup',
     displayName: 'Sign Up',
@@ -71,17 +79,26 @@ const mapSignup = (state) => {
   }
 }
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = dispatch => {
   return {
-    handleSubmit (evt) {
+    handleSubmit(evt) {
       evt.preventDefault()
+      //signup / signin
       const formName = evt.target.name
-      const firstName = evt.target.firstName.value
-      const lastName = evt.target.lastName.value
-      const phoneNumber = evt.target.phoneNumber.value
       const email = evt.target.email.value
       const password = evt.target.password.value
-      dispatch(auth(firstName, lastName, phoneNumber, email, password, formName))
+
+      let firstName = ''
+      let lastName = ''
+      let phoneNumber = ''
+      if (formName === 'signup') {
+        firstName = evt.target.firstName.value
+        lastName = evt.target.lastName.value
+        phoneNumber = evt.target.phoneNumber.value
+      }
+      dispatch(
+        auth(firstName, lastName, phoneNumber, email, password, formName)
+      )
     }
   }
 }
