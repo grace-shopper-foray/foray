0f533b261c1e695b7584e621b9a328efd4823205
Philip_Leung <philip.leung90@gmail.com>
2018-04-21 08:35:30 -0400

    added styling on my account page

--- client/components/account.js
+++ client/components/account.js
@@ -1,22 +1,61 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 /**
  * COMPONENT
  */
-export const Account = (props) => {
-  const {user} = props
+export const Account = props => {
+  const { user } = props
 
   return (
-    <div>
-      <h3>Welcome, {user.firstName} {user.lastName}</h3>
-      <h4>Account Information</h4>
-      <ul>
-        <li>Email Address: {user.email}</li>
-        <li>Phone Number: {user.phoneNumber}</li>
-        <li>Order History: TBD</li>
-      </ul>
+    <div className="container text-center">
+      <div className="row">
+        <div className="container-fluid well span6">
+          <h4>Account Information</h4>
+          <div className="row-fluid">
+            <div className="span2">
+              <img src="" className="img-circle" />
+            </div>
+
+            <div className="span8">
+              <h3>
+                Welcome, {user.firstName} {user.lastName}
+              </h3>
+              <h6>Email: {user.email}</h6>
+              <h6>Phone Number: {user.phoneNumber}</h6>
+              <div>
+                <h6>Order History</h6>
+              </div>
+            </div>
+            <div className="dropdown show">
+              <a
+                className="btn btn-secondary dropdown-toggle"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Action
+              </a>
+
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a className="dropdown-item" href="#">
+                  Edit
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
@@ -24,7 +63,7 @@ export const Account = (props) => {
 /**
  * CONTAINER
  */
-const mapState = (state) => {
+const mapState = state => {
   return {
     user: state.user
   }
