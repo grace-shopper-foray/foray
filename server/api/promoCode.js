602b69e0422d95d848a9484ea50a7b2bb57f04ca
Philip_Leung <philip.leung90@gmail.com>
2018-04-22 19:19:49 -0400

    done setting up promoCode route

--- server/api/promoCode.js
+++ server/api/promoCode.js
@@ -4,20 +4,21 @@ const { PromoCode } = require('../db/models')
 // get all promo Code
 
 // find active promo code
-router.get('/', (req, res, next) => {
+router.get('/:promoCode', (req, res, next) => {
   console.log('in the pormo Code')
-  const { promoCode } = req.body //cant use body for get
+  const promoCode = req.params.promoCode
+  console.log(promoCode)
   PromoCode.findOne({
     where: {
       name: promoCode,
       isActive: true
     }
-      .then(result => {
-        console.log(result)
-        res.status(200).json(result)
-      })
-      .catch(next)
   })
+    .then(result => {
+      if (result) res.status(200).json(result)
+      else res.sendStatus(404)
+    })
+    .catch(next)
 })
 
 module.exports = router
