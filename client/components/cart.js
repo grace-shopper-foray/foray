06de1b9178af1ad8c7db73243638ad9efdd261f3
Philip_Leung <philip.leung90@gmail.com>
2018-04-22 12:02:16 -0400

    fixed problem adding trip more than 5 to cart

--- client/components/cart.js
+++ client/components/cart.js
@@ -108,6 +108,11 @@ export class Cart extends React.Component {
                               <option value="3">3</option>
                               <option value="4">4</option>
                               <option value="5">5</option>
+                              <option value="6">6</option>
+                              <option value="7">7</option>
+                              <option value="8">8</option>
+                              <option value="9">9</option>
+                              <option value="10">10</option>
                             </select>
                           </td>
                           <td className="actions" data-th="">
