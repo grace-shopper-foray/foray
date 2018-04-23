b2e8ae0bd68c70ff8f37eb69726cea8d7deefe2e
BxBomber999 <coyle.peters@gmail.com>
2018-04-23 09:44:32 -0400

    clean-up db_models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,22 +1,14 @@
-const User = require('./user')
-const Trip = require('./trip')
-const Order = require('./order')
-const TripOrder = require('./tripOrder')
-const db = require('../db')
+const User = require('./user');
+const Trip = require('./trip');
+const Order = require('./order');
+const TripOrder = require('./tripOrder');
+const db = require('../db');
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- *
- **/
+Trip.belongsToMany(Order, { through: TripOrder });
+Order.belongsToMany(Trip, { through: TripOrder });
 
-Trip.belongsToMany(Order, { through: TripOrder })
-Order.belongsToMany(Trip, { through: TripOrder })
-
-Order.belongsTo(User)
-User.hasMany(Order)
+Order.belongsTo(User);
+User.hasMany(Order);
 
 module.exports = {
   User,
@@ -24,4 +16,4 @@ module.exports = {
   Order,
   TripOrder,
   db
-}
+};
--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,26 +1,24 @@
-const Sequelize = require('sequelize')
-const TripOrder = require('./tripOrder')
-const db = require('../db')
-
+const Sequelize = require('sequelize');
+const TripOrder = require('./tripOrder');
+const db = require('../db');
 
 const Order = db.define('order', {
-    isCheckedOut: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false
-    }
-})
+  isCheckedOut: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+});
 
 Order.prototype.totalPrice = () => {
-    return TripOrder.findAll({
-        where: {
-            orderId: this.getDataValue('id')
-        }
-    })
-    .then((tripOrderArr) => {
-        return tripOrderArr.reduce((acc, tripOrder) => {
-                return acc + tripOrder.pricePerTrip
-        })
-    })
-}
+  return TripOrder.findAll({
+    where: {
+      orderId: this.getDataValue('id')
+    }
+  }).then(tripOrderArr => {
+    return tripOrderArr.reduce((acc, tripOrder) => {
+      return acc + tripOrder.pricePerTrip;
+    });
+  });
+};
 
-module.exports = Order
+module.exports = Order;
