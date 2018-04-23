150710c6ff1fb5995e165b2a17e75dbcd72e38cd
BxBomber999 <coyle.peters@gmail.com>
2018-04-23 12:50:38 -0400

    removed placeholder files from clean branch

--- server/db/models/trip.spec.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require('chai');
-const db = require('../index');
-const Trip = db.model('trip');
-
-describe('Trip Model', () => {
-  beforeEach(() => {
-    return db.sync({ force: true });
-  });
-});
