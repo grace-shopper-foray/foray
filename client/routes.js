05dc50a112c77a67768fff7978ef5a2e2f5a139f
BxBomber999 <coyle.peters@gmail.com>
2018-04-25 02:00:42 -0400

    clean frontent, functionality tested and working

--- client/routes.js
+++ client/routes.js
@@ -1,13 +1,7 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import {
-  BrowserRouter,
-  withRouter,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import {
   Login,
   Signup,
@@ -18,20 +12,20 @@ import {
   Checkout,
   OrderHistoryDetail,
   EditAccount
-} from './components'
-import { me, fetchTrips, fetchOrder } from './store'
+} from './components';
+import { me, fetchTrips, fetchOrder } from './store';
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
   componentDidMount() {
-    this.props.loadInitialData()
-    this.props.fetchTripsFromServer()
+    this.props.loadInitialData();
+    this.props.fetchTripsFromServer();
   }
 
   render() {
-    const { isLoggedIn } = this.props
+    const { isLoggedIn } = this.props;
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
@@ -59,7 +53,7 @@ class Routes extends Component {
           </Switch>
         )}
       </Switch>
-    )
+    );
   }
 }
 
@@ -68,30 +62,26 @@ class Routes extends Component {
  */
 const mapState = state => {
   return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
     isLoggedIn: !!state.user.id,
     user: state.user
-  }
-}
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
     loadInitialData() {
-      dispatch(me())
+      dispatch(me());
     },
     fetchTripsFromServer: function() {
-      return dispatch(fetchTrips())
+      return dispatch(fetchTrips());
     },
     fetchOrderfromServer: function(userId) {
-      return dispatch(fetchOrder(userId))
+      return dispatch(fetchOrder(userId));
     }
-  }
-}
+  };
+};
 
-// The `withRouter` wrapper makes sure that updates are not blocked
-// when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+export default withRouter(connect(mapState, mapDispatch)(Routes));
 
 /**
  * PROP TYPES
@@ -99,4 +89,4 @@ export default withRouter(connect(mapState, mapDispatch)(Routes))
 Routes.propTypes = {
   loadInitialData: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
-}
+};
